9bcd452a917f7dcef51eb5506c58607b35453f75
Alexandre Bury <alexandre.bury@gmail.com>
2018-10-19 15:09:11 -0700

    Replace tomllib with toml_edit

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,35 @@
 extern crate clap;
 extern crate semver;
-extern crate tomllib;
+extern crate toml_edit;
 
 mod config;
 mod version;
 
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
-use std::path::Path;
-
 use semver::Version;
-use tomllib::types::{ParseResult, Value};
-use tomllib::TOMLParser;
+use toml_edit::Document;
+//use tomllib::types::{ParseResult, Value};
+//use tomllib::TOMLParser;
 
 fn main() {
     let conf = config::get_config();
-    let raw_data = read_file(&conf.manifest);
-    let parser = TOMLParser::new();
-    let (mut parser, result) = parser.parse(&raw_data);
-    match result {
-        ParseResult::Full => {}
-        _ => panic!("couldn't parse Cargo.toml"),
-    }
+    let raw_data = std::fs::read_to_string(&conf.manifest).unwrap();
 
-    let raw_value = parser
-        .get_value("package.version")
-        .expect("package.version missing");
-    let mut version = match raw_value {
-        Value::String(raw_version, _) => Version::parse(&raw_version).unwrap(),
-        _ => panic!("version not a string"),
-    };
+    let mut result = raw_data
+        .parse::<Document>()
+        .expect("could not parse Cargo.toml");
+
+    let mut version = Version::parse(
+        result["package"]["version"]
+            .as_str()
+            .expect("version is not a string"),
+    )
+    .unwrap();
 
     let old_version = version.clone();
     version::update_version(&mut version, conf.version, conf.pre_release, conf.metadata);
     println!("Version {} -> {}", old_version, version);
 
-    parser.set_value(
-        "package.version",
-        Value::basic_string(version.to_string()).unwrap(),
-    );
+    *result["package"]["version"].as_value_mut().unwrap() = version.to_string().into();
 
-    let mut f = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(&conf.manifest)
-        .unwrap();
-    f.write_all(format!("{}", parser).as_bytes()).unwrap();
-}
-
-fn read_file(file: &Path) -> String {
-    let mut file = File::open(file).unwrap();
-    let mut raw_data = String::new();
-    file.read_to_string(&mut raw_data).unwrap();
-    raw_data
+    std::fs::write(&conf.manifest, result.to_string()).unwrap();
 }
